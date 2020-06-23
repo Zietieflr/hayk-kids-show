bde4c12d24d55494a8266ff0bf11bf8279a4eca5
Zietieflr <lrmcguire93@gmail.com>
2020-06-23 10:29:01 -0600

    Lesson complete.

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,2 +1,4 @@
 class Kid < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { in: 7..25 }
+  validates :age, presence: true, numericality: { greater_than_or_equal_to: 6, less_than_or_equal_to: 15 }
 end
